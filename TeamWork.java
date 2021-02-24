8b21737670684568361393369a9ed5492c49ba5f
Jheave <jimenezjheave123@gmail.com>
2021-02-24 22:25:54 +0800

    fix

--- TeamWork.java
+++ TeamWork.java
@@ -3,6 +3,13 @@ package Recursion;
 import java.util.Scanner;
 public class TeamWork {
     // n = number of disk to be move rodOne where the disk is present rodThree where the disk to be move rodTwo is the remaining one 
+    static void Hanoi(int n, char rodOne, char rodThree, char rodTwo) 
+    { 
+        //lawrence
+        
+ 
+    } 
+     
     public static void main(String args[]) 
     { 
         // Ask the User of Number of disks 
